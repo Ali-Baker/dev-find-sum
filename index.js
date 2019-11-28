d06cc198bea8bdeb69929af8862b7c8e1ee3cc04
Ali Ali <ali.ali@immediate.co.uk>
2019-11-28 14:23:51 +0000

    refactor: refactor

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const method = (unSortedArr, originalSum) => {
   const results = [];
 
   let sortedArr = unSortedArr.sort((a, b) => a-b);
-  let counter = 0;
-  const maxTry = Math.pow(sortedArr.length, sortedArr.length);
+  let counter = sortedArr.length;
 
   const arrSum = arr => arr.reduce((a,b) => a + b, 0);
 
-  while(currentSum !== originalSum && currentSum < originalSum && counter <= maxTry) {
+  while(currentSum !== originalSum && currentSum < originalSum && counter > 0) {
     let l = 0, r = sortedArr.length - 1;
     for (let i = 0; i < sortedArr.length; i++) {
       if (sortedArr[l] + sortedArr[r] === sum) {
@@ -17,25 +16,30 @@ const method = (unSortedArr, originalSum) => {
         results.push(sortedArr[r]);
         currentSum = currentSum + sortedArr[l] + sortedArr[r];
         sortedArr.splice(l, 1).splice(r-1 , 1);
+        counter = sortedArr.length;
         break;
       }
-      else if(sortedArr[l] === sum) {
+      else if(sortedArr[l] === sum || l === r) {
         results.push(sortedArr[l]);
         currentSum = currentSum + sortedArr[l];
         sortedArr.splice(l, 1);
+        counter = sortedArr.length;
         break;
       }
       else if(sortedArr[r] === sum) {
         results.push(sortedArr[r]);
         currentSum = currentSum + sortedArr[r];
         sortedArr.splice(r, 1);
+        counter = sortedArr.length;
         break;
-      } else if (i === sortedArr.length - 1) {
+      }
+      else if (i === sortedArr.length - 1) {
         results.push(sortedArr[l]);
         results.push(sortedArr[r]);
         currentSum = currentSum + sortedArr[l] + sortedArr[r];
         sortedArr.splice(l, 1).splice(r-1 , 1);
         sum = sum - currentSum;
+        counter = sortedArr.length;
         break;
       }
 
@@ -43,7 +47,7 @@ const method = (unSortedArr, originalSum) => {
       sortedArr[l] + sortedArr[r] < sum && l++;
     }
 
-    counter++;
+    counter--;
   }
 
 
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const findSumArray = require('./index');
 
 const arrSum = arr => arr.reduce((a,b) => a + b, 0);
-const maxArrayLength = 10000000;
+const maxArrayLength = 10000;
 const sum = Math.ceil(Math.random() * maxArrayLength);
 
 const arr = Array.from({length: maxArrayLength}, () => Math.ceil(Math.random() * maxArrayLength));
