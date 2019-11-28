180b59eb6895e20d2ea32200e624119b2825c311
Ali Ali <ali.ali@immediate.co.uk>
2019-11-28 14:28:42 +0000

    test: add can not find solution test

--- test.js
+++ test.js
@@ -2,14 +2,19 @@ const findSumArray = require('./index');
 
 const arrSum = arr => arr.reduce((a,b) => a + b, 0);
 const maxArrayLength = 10000;
-const sum = Math.ceil(Math.random() * maxArrayLength);
 
-const arr = Array.from({length: maxArrayLength}, () => Math.ceil(Math.random() * maxArrayLength));
-
-test('return an array of numbers', () => {
+test('it return an array of numbers', () => {
+    const sum = Math.ceil(Math.random() * maxArrayLength);
+    const arr = Array.from({length: maxArrayLength}, () => Math.ceil(Math.random() * maxArrayLength));
     const result = findSumArray(arr, sum);
+
     expect(arrSum(result)).toBe(sum);
 });
 
 
-
+test('it return empty array when it can not find solution', () => {
+    const sum = 113;
+    const result = findSumArray([1,2,3], sum);
+    
+    expect(arrSum(result)).not.toBe(sum);
+});
